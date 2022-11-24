62e01057d78793fae39b7846f17334da8f2544c7
Dmitrii <dvgaluta@gmail.com>
2022-11-25 00:15:59 +0300

    Commit_36

--- backend/views.py
+++ backend/views.py
@@ -165,8 +165,6 @@ class ShopView(ListAPIView):
     """
     Класс для просмотра списка магазинов
     """
-    # новая строка для записи нового магазина.
-    # Shop.objects.create(name='superkonekt', url='', state=True, filename='')
     queryset = Shop.objects.filter(state=True)
     serializer_class = ShopSerializer
 
@@ -296,67 +294,62 @@ class PartnerUpdate(APIView):
     Класс для обновления прайса от поставщика
     """
     def post(self, request, *args, **kwargs):
-        # if not request.user.is_authenticated:
-        #     return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
-        #
-        # if request.user.type != 'shop':
-        #     return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
 
-        # url = request.data.get('url')
+        if request.user.type != 'shop':
+            return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
+
+        url = request.data.get('url')
         # path_fail = request.data.get('path')
-        # path_fail = '/home/ubunta/homeworks/Diplom/my_diplom/Data/shop1.yaml'
-        path_fail_row = Path(pathlib.Path.cwd(), "Data", "shop1.yaml")
-        path_fail = str(path_fail_row)
-        print(path_fail)
-        # if url:
-        #     validate_url = URLValidator()
-        #     try:
-        #         validate_url(url)
-        #     except ValidationError as e:
-        #         return JsonResponse({'Status': False, 'Error': str(e)})
-        if isinstance(path_fail, str):
+        # path_fail_row = Path(pathlib.Path.cwd(), "Data", "shop1.yaml")
+        # path_fail = str(path_fail_row)
+        # print(path_fail)
+        if url:
+            validate_url = URLValidator()
+            try:
+                validate_url(url)
+            except ValidationError as e:
+                return JsonResponse({'Status': False, 'Error': str(e)})
+        # if isinstance(path_fail, str):
                 # try:
                      # print('файл грузится')
 
                 # except ValidationError as e:
                 #     return JsonResponse({'Status': False, 'Error': str(e)})
 
-             # else:
-                # stream = get(url).content  # specify path to YML file
-                # data = load_yaml(stream, Loader=Loader)
-
-            with open(f"{path_fail}", 'r') as stream:
+            else:
+                stream = get(url).content  # specify path to YML file
                 data = load_yaml(stream, Loader=Loader)
-                print(data)
 
-            shop, _ = Shop.objects.get_or_create(name=data['shop'])
+            # with open(f"{path_fail}", 'r') as stream:
+            #     data = load_yaml(stream, Loader=Loader)
+            #     print(data)
 
-        # пока такую запись (name=data['shop'], user_id=request.user.id)  поменял
+                shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
+                for category in data['categories']:
+                    category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+                    category_object.shops.add(shop.id)
+                    category_object.save()
+                ProductInfo.objects.filter(shop_id=shop.id).delete()
+                for item in data['goods']:
+                    product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
 
-            for category in data['categories']:
-                category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
-                category_object.shops.add(shop.id)
-                category_object.save()
-            ProductInfo.objects.filter(shop_id=shop.id).delete()
-            for item in data['goods']:
-                product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+                    product_info = ProductInfo.objects.create(product_id=product.id,
+                                                              name=item['name'],
+                                                              external_id=item['id'],
+                                                              price=item['price'],
+                                                              price_rrc=item['price_rrc'],
+                                                              quantity=item['quantity'],
+                                                              shop_id=shop.id)
+                    for name, value in item['parameters'].items():
+                        parameter_object, _ = Parameter.objects.get_or_create(name=name)
+                        ProductParameter.objects.create(product_info_id=product_info.id,
+                                                        parameter_id=parameter_object.id,
+                                                        value=value)
 
-                product_info = ProductInfo.objects.create(product_id=product.id,
-                                                          name=item['name'],
-                                                          external_id=item['id'],
-                                                          price=item['price'],
-                                                          price_rrc=item['price_rrc'],
-                                                          quantity=item['quantity'],
-                                                          shop_id=shop.id)
-                for name, value in item['parameters'].items():
-                    parameter_object, _ = Parameter.objects.get_or_create(name=name)
-                    ProductParameter.objects.create(product_info_id=product_info.id,
-                                                    parameter_id=parameter_object.id,
-                                                    value=value)
+                return JsonResponse({'Status': True})
 
-            return JsonResponse({'Status': True})
-
-        # return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})external_id=item['id']
         return JsonResponse({'Status': False, 'Errors': 'Не верно указан путь к файлу'})
 
 
