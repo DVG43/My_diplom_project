f4dc9bdac3deb15853861df6cc7d8a03a9c4f782
Dmitrii <dvgaluta@gmail.com>
2022-12-07 23:18:16 +0300

    Commit_47_исправил_settings_в части блока работы с отправкой писем

--- orders/settings.py
+++ orders/settings.py
@@ -144,11 +144,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = '2525'
 EMAIL_HOST_USER = 'santuk@mail.ru'
-EMAIL_HOST_PASSWORD = '**************'
-EMAIL_PORT = '465'
-EMAIL_USE_SSL = True
+EMAIL_HOST_PASSWORD = '******'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
